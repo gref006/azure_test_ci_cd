7c84bdf3559d86d7ad16bb19c97a03ee15ca4d9c
Pavel Hujer <hujer@wwm-labs.com>
2025-11-10 17:19:28 +0100

    Add automatic update scripts

---
+++ GUI/updateDevEnv.js
@@ -0,0 +1,37 @@
+import fs from 'fs/promises';
+import path from 'path';
+import {DateTime} from "luxon";
+
+const envFileNames = ['.env.development'];
+
+const currentTimestamp = DateTime.now().toFormat('yyyy-MM-dd HH:mm');
+
+async function updateDevEnvFiles() {
+  for (const fileName of envFileNames) {
+    const envFilePath = path.join(process.cwd(), fileName);
+
+    try {
+      await fs.access(envFilePath);
+
+      let data = await fs.readFile(envFilePath, 'utf8');
+
+      const updatedEnvFileContent = data.includes('VITE_APP_VERSION')
+        ? data.replace(
+            /VITE_APP_VERSION=.*/,
+            `VITE_APP_VERSION=${currentTimestamp}`,
+          )
+        : `${data.trim()}\nVITE_APP_VERSION=${currentTimestamp}\n`;
+
+      await fs.writeFile(envFilePath, updatedEnvFileContent, 'utf8');
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        console.error(`File ${fileName} does not exist. Skipping update.`);
+      } else {
+        console.error(`Error updating ${fileName} file:`, err);
+        process.exit(1);
+      }
+    }
+  }
+}
+
+updateDevEnvFiles();
