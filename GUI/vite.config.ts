25bcafbd21d542e5dfb831ca000a03bcd9e704f5
Pavel Hujer <hujer@wwm-labs.com>
2025-11-10 17:19:44 +0100

    Adjust vite config

--- GUI/vite.config.ts
+++ GUI/vite.config.ts
@@ -1,7 +1,70 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    build: { outDir: '../Backend/API/wwwroot' },
+    server: {
+        port: 3000,
+    },
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            injectRegister: 'auto',
+            devOptions: {
+                enabled: true,
+            },
+
+            manifest: {
+                name: 'ARMS - Admin',
+                short_name: 'ARMS',
+                description: 'ARMS Admin Application',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: '/assets/icon.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/assets/icon192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/assets/icon.png',
+                        sizes: '512x512',
+                        type: 'any',
+                    },
+                    {
+                        src: '/assets/icon.png',
+                        sizes: '512x512',
+                        type: 'maskable',
+                    },
+                ],
+            },
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+                clientsClaim: true,
+                skipWaiting: true,
+            },
+        }),
+    ],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/tests/setupTest.ts',
+        isolate: true,
+        testTimeout: 600000,
+
+        fileParallelism: true,
+        maxConcurrency: 2,
+        allowOnly: false,
+        include: ["src/**/*.test.{ts,tsx}"],
+
+        poolOptions: {
+            threads: { singleThread: false },
+        },
+    },
+});
